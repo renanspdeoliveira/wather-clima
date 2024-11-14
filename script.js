9778b9c06e0034f18a9883c28dab6b30c97264a8
Renan Oliveira <56898065+renanspdeoliveira@users.noreply.github.com>
2024-11-14 09:01:12 -0300

    editbackground

---
+++ script.js
@@ -0,0 +1,57 @@
+const apiKey = "b1cb07def126c77f5f3106f72be47968";
+//const apiCountryURL = "https://flagsapi.com/ "
+
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+
+const cityElement = document.querySelector("#city")
+const tempElement = document.querySelector("#temperature span")
+const descElement = document.querySelector("#description")
+const weatherIconElement = document.querySelector("#weather-icon")
+const countryElement = document.querySelector("#country")
+const humidityElement = document.querySelector("#umidity span")
+const windElement = document.querySelector("#wind span")
+
+
+const getWeatherData = async (city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    return data;
+
+};
+
+const showWeatherData = async (city) => {
+
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+    //countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+};
+
+
+
+searchBtn.addEventListener("click", (e) => {
+
+    e.preventDefault()
+
+    const city = cityInput.value
+
+
+
+    showWeatherData(city);
+
+
+
+})
